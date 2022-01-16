4eb7e5db10b60ed33e94be16906dcc1e88740d6b
Nanook15 <nanook.eth@protonmail.com>
2022-01-16 13:56:42 +0000

    Update 16.01.22

---
+++ basics/ex1.rb
@@ -0,0 +1,11 @@
+# "<Firstname> <Lastname>"
+
+first_name = "Bob"
+
+last_name = "Saget"
+
+
+puts first_name + " " + last_name			# string concatenaton
+
+puts "Hi my name is #{first_name} #{last_name}"		# string interpolation
+
